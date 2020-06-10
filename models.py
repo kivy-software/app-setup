de1b8193d3d62364a5ef09bc3f570c9a9fbfa0d3
Daniel Miliński <daniel.milinski@kivy.pl>
2020-06-10 20:57:59 +0200

    WIP

--- models.py
+++ models.py
@@ -1,11 +1,13 @@
 # {{ app_name }}/models.py
 from django.db import models
 
-# from .managers import {{ single_app_name }}
+# from .managers import {{ single_app_name }}Manager
 
 
 class {{ single_app_name }}(models.Model):
     # name = models.CharField(max_length=150, unique=True)
+
+    # objects = {{ single_app_name }}Manager()
 
     class Meta:
         ordering = ("-id", )
