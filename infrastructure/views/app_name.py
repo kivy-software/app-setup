d8d0a15b2f0ff1489996504977a98d1fb9d2d641
Daniel Miliński <daniel.milinski@kivy.pl>
2024-02-01 15:24:28 +0100

    adde paths

--- infrastructure/views/app_name.py
+++ infrastructure/views/app_name.py
@@ -1,13 +1,13 @@
 # {{toplevel}}/infrastructure/views/{{ app_name }}/{{ single_app_name }}.py
 from rest_framework import viewsets
 
-# from .filters import {{ single_app_name }}Filter
-from .models import {{ single_app_name }}
-from .serializers import {{ single_app_name }}Serializer
+from {{toplevel}}.{{ app_name }}.infrastructure.filters import {{ single_app_name }}Filter
+from {{toplevel}}.{{ app_name }}.infrastructure.models import {{ single_app_name }}
+from {{toplevel}}.{{ app_name }}.infrastructure.serializers import {{ single_app_name }}Serializer
 
 
 class {{ single_app_name }}ViewSet(viewsets.ModelViewSet):
     queryset = {{ single_app_name }}.objects.all()
     serializer_class = {{ single_app_name }}Serializer
     ordering_fields = ("id", )
-    # filter_class = {{ single_app_name }}Filter
+    filterset_class = {{ single_app_name }}Filter
