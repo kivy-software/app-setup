1a2bc4bb09fd691f5d33be5c5f15e41ae0398955
Daniel Miliński <daniel.milinski@kivy.pl>
2024-02-01 13:36:19 +0100

    added date

---
+++ infrastructure/querysets/__init__.py
@@ -0,0 +1,2 @@
+# {{ top_level }}/{{ app_name }}/infrastructure/querysets/__init__.py
+from .{{ app_name }} import {{ single_app_name }}QuerySet
---
+++ infrastructure/querysets/app_name.py
@@ -0,0 +1,6 @@
+# {{ top_level }}/{{ app_name }}/infrastructure/querysets/{{ single_app_name }}.py
+from django.db.models import QuerySet
+
+
+class {{ single_app_name }}QuerySet(QuerySet):
+    pass
