ff40db8d603cbd6f6ca27941275f7d8b2b87bb5a
Daniel Miliński <daniel.milinski@kivy.pl>
2024-02-01 13:07:23 +0100

    delete

--- querysets.py
+++
@@ -1,6 +0,0 @@
-# {{ top_level }}/infrastructure/querysets/{{ single_app_name }}/{{ single_app_name }}.py
-from django.db.models import QuerySet
-
-
-class {{ single_app_name }}QuerySet(QuerySet):
-    pass
